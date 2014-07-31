0193266468b21bbfaa7cc65ca1553db4f297cbb9
Thomas Levine <_@thomaslevine.com>
2014-07-31 07:53:14 +0000

    features

--- awttje.hs
+++ awttje.hs
@@ -3,3 +3,23 @@ import qualified Data.Map as M
 data Level = Nominal | Ordinal | Interval | Ratio
 type Column = [String]
 type Histogram a = M.Map a Integer
+type Feature a = String -> a
+
+nChar :: Feature Integer
+nChar = length
+
+nNonNumericChar :: Feature Integer
+nNonNumericChar x = length $ filter (notElem "0123456789.,") x
+
+
+data Sign = Increasing | Decreasing
+
+signedSorted :: Sign -> Column -> Bool
+signedSorted _ [] = True
+signedSorted _ x:[] = True
+signedSorted Increasing x:y:[] = x <= y
+signedSorted Decreasing x:y:[] = x => y
+signedSorted sign x:y:zs = signedSorted sign [x,y] && signedSorted sign y:zs
+
+sorted :: Column -> Bool
+sorted = signedSorted Increasing
